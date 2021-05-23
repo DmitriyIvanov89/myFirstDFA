47d500d99ebcd4b66b23f9150ba0564cbdce65e3
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-05-23 14:00:17 +0300

    some changes

--- src/main/java/Validatorforemail/DFAValidator.java
+++ src/main/java/Validatorforemail/DFAValidator.java
@@ -4,31 +4,8 @@ public class DFAValidator {
 
     private State startState;
 
-    public void init() {
-        State s0 = new State("S0", false);
-        State s1 = new State("S1", false);
-        State s2 = new State("S2", false);
-        State s3 = new State("S3", false);
-        State s4 = new State("S4", false);
-        State s5 = new State("S5", false);
-        State s6 = new State("S6", true);
-        State s7 = new State("S7", true);
-        State s8 = new State("S8", true);
-        State s9 = new State("S9", true);
-        startState = s0;
-
-        addTransitionsByAllLetters(s0, s1);
-        s1.addTransition('.', s0);
-        addTransitionsByAllLetters(s1, s1);
-        s1.addTransition('@', s2);
-        addTransitionsByAllLetters(s2, s3);
-        addTransitionsByAllLetters(s3, s3);
-        s3.addTransition('.', s4);
-        addTransitionsByAllLetters(s4, s5);
-        addTransitionsByAllLetters(s5, s6);
-        addTransitionsByAllLetters(s6, s7);
-        addTransitionsByAllLetters(s7, s8);
-        addTransitionsByAllLetters(s8, s9);
+    public DFAValidator(DFAConfig config) {
+        
     }
 
     public boolean validate(String str) {
